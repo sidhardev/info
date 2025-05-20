ffc6165f5102e232ad265f3dd7994528dd2b4b1c
Dev Sidhar <64716772+sidhardev@users.noreply.github.com>
2025-05-20 05:26:02 +0000

    creATED

---
+++ script.js
@@ -0,0 +1,4 @@
+// Your future interesting JavaScript code will go here!
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Portfolio script loaded and DOM fully parsed!");
+});
